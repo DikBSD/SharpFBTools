c19964997d4049fe1c4d8f1d38d523a659719c39
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 08:46:24 +0000

    Менеджер Файлов: + Description: класс History

---
+++ Source/FB2/Description/DocumentInfo/History.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 12:44
+ * 
+ * License: GPL 2.1
+ */
+using System;
+using FB2.Description.TitleInfo;
+
+namespace FB2.Description.DocumentInfo
+{
+	/// <summary>
+	/// Description of History.
+	/// </summary>
+	public class History : Annotation
+	{
+		public History()
+		{
+		}
+	}
+}
