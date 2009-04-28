f6701bb5a3a3e328b5128584f997d0d0034a8bac
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 13:28:17 +0000

    Менеджер Файлов: Начал делать парсеры

---
+++ Source/FB2/FB2Parsers/FB2DescriptionParser.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 17:21
+ * 
+ * License: GPL 2.1
+ */
+using System;
+
+namespace FB2.FB2Parsers
+{
+	/// <summary>
+	/// Description of FB2DescriptionParser.
+	/// </summary>
+	public class FB2DescriptionParser
+	{
+		public FB2DescriptionParser()
+		{
+		}
+	}
+}
---
+++ Source/FB2/FB2Parsers/IFB2Parser.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 17:23
+ * 
+ * License: GPL 2.1
+ */
+using System;
+using System.Xml;
+
+namespace FB2.FB2Parsers
+{
+	/// <summary>
+	/// Description of IFB2Parser.
+	/// </summary>
+	public interface IFB2Parser
+    {
+        XmlDocument RawData { get; }
+        //IFBDocument Parse( string sFB2Path );
+    }
+}
