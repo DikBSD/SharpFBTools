2f756c1f360260206207da583e744ad82a62d789
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 12:58:57 +0000

    Менеджер Файлов: + Description: класс Coverpage

---
+++ Source/FB2/Description/TitleInfo/Coverpage.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 16:56
+ * 
+ * License: GPL 2.1
+ */
+using System;
+using FB2.Common;
+
+namespace FB2.Description.TitleInfo
+{
+	/// <summary>
+	/// Description of Coverpage.
+	/// </summary>
+	public class Coverpage : Image
+	{
+		public Coverpage()
+		{
+		}
+	}
+}
