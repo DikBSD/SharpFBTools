9edc6a760103725029143219551ea999795729ba
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 05:27:13 +0000

    Менеджер Файлов: + Description: Автор

--- Source/FB2/Description/Common/Author.cs
+++ Source/FB2/Description/Common/Author.cs
@@ -19,13 +19,13 @@ namespace FB2.Description.Common
 	public class Author : IComparable
 	{
 		#region Закрытые данные класса
-        private string m_sFirstName;
-        private string m_sMiddleName;
-        private string m_sLastName;
-        private string m_sNickName;
+        private string	m_sFirstName;
+        private string	m_sMiddleName;
+        private string	m_sLastName;
+        private string	m_sNickName;
         private IList<string> m_lisHomePages;
         private IList<string> m_lisEmails;
-        private string m_sID;
+        private string	m_sID;
 		#endregion
 		
 		#region Закрытые Вспомогательные методы класса
@@ -40,14 +40,106 @@ namespace FB2.Description.Common
         }
 		#endregion
 		
+		#region Открытые Вспомогательные методы класса
+		public int CompareTo( object o ) {
+            if( o.GetType() != typeof( Author ) ) {
+                throw new ArgumentException("the object type is not Author");
+            }
+            return ( ID == ( (Author)o ).ID ) ? 0 : -1;
+        }
+
+		#endregion
+		
 		#region Конструкторы класса
 		public Author()
 		{
 		}
+		public Author( string sFirstName, string sMiddleName, string sLastName, string sNickName,
+		              IList<string> lisHomePages, IList<string> lisEmails, string sID )
+        {
+            // все данные об Авторе
+			m_sFirstName	= sFirstName;
+            m_sMiddleName	= sMiddleName;
+            m_sLastName		= sLastName;
+            m_sNickName		= sNickName;
+            m_lisHomePages	= lisHomePages;
+            m_lisEmails		= lisEmails;
+            m_sID			= sID;
+        }
+		public Author( string sFirstName, string sMiddleName, string sLastName, string sNickName,
+		              IList<string> lisHomePages, IList<string> lisEmails )
+        {
+            // все данные об Авторе без его id
+			m_sFirstName	= sFirstName;
+            m_sMiddleName	= sMiddleName;
+            m_sLastName		= sLastName;
+            m_sNickName		= sNickName;
+            m_lisHomePages	= lisHomePages;
+            m_lisEmails		= lisEmails;
+        }
+		public Author( string sFirstName, string sMiddleName, string sLastName, string sID )
+        {
+            // только ФИО Автора
+			m_sFirstName	= sFirstName;
+            m_sMiddleName	= sMiddleName;
+            m_sLastName		= sLastName;
+        }
+		public Author( string sFirstName, string sLastName )
+        {
+            // только Имя и Фамилия Автора
+			m_sFirstName	= sFirstName;
+            m_sLastName		= sLastName;
+        }
+		public Author( string sNickName )
+        {
+            // только Ник Автора
+			m_sNickName = sNickName;
+        }
 		#endregion
 		
 		#region Открытые свойства-fb2-элементы класса
-		
+		public virtual string FirstName {
+            get { return m_sFirstName; }
+            set { m_sFirstName = value; }
+        }
+
+        public virtual string MiddleName {
+            get { return m_sMiddleName; }
+            set { m_sMiddleName = value; }
+        }
+
+        public virtual string LastName {
+            get { return m_sLastName; }
+            set { m_sLastName = value; }
+        }
+
+        public virtual string NickName {
+            get { return m_sNickName; }
+            set { m_sNickName = value; }
+        }
+
+        public virtual IList<string> HomePages {
+            get { return m_lisHomePages; }
+            set { m_lisHomePages = value; }
+        }
+
+        public virtual IList<string> EMails {
+            get { return m_lisEmails; }
+            set { m_lisEmails = value; }
+        }
+
+        public virtual string ID {
+            get {
+                if( m_sID == null ) {
+                    m_sID = CalculateMD5Hash( String.Format("{0} {1} {2} {3}", FirstName, MiddleName, LastName, NickName ).ToLower() );
+                }
+                return m_sID;
+            } set {
+                if( m_sID == null ) {
+                    m_sID = value;
+                }
+            }
+        }
 		#endregion
 	}
 }
