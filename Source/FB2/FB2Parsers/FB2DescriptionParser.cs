9f253b66c4ef02f41b602e6beb4de27fcad6e5db
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 13:46:39 +0000

    Менеджер Файлов: Начал делать парсеры

--- Source/FB2/FB2Parsers/FB2DescriptionParser.cs
+++ Source/FB2/FB2Parsers/FB2DescriptionParser.cs
@@ -7,6 +7,8 @@
  * License: GPL 2.1
  */
 using System;
+using System.Xml;
+using FB2.Description;
 
 namespace FB2.FB2Parsers
 {
@@ -15,8 +17,31 @@ namespace FB2.FB2Parsers
 	/// </summary>
 	public class FB2DescriptionParser
 	{
-		public FB2DescriptionParser()
-		{
-		}
+		#region Закрытые данные класса
+		private XmlNamespaceManager m_NsManager;
+        private XmlDocument			m_RawData;
+        #endregion
+        
+		#region Конструкторы класса
+        public FB2DescriptionParser()
+        {
+            m_RawData = new XmlDocument();
+        }
+        #endregion
+        
+        #region Открытые свойства класса
+        public XmlDocument RawData
+        {
+            get { return m_RawData; }
+        }
+        #endregion
+        
+        #region Открытые методы класса
+        public Description.Description Parse( string sFB2Path )
+        {
+        	/// TODO доделать
+        	return new Description.Description();
+        }
+        #endregion
 	}
 }
