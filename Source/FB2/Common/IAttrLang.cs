cb43bc3d5c15a9d4627a15a2fdaadd4f581e8362
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2009-04-28 07:13:51 +0000

    Менеджер Файлов: 2 интерфейса - lang и value

---
+++ Source/FB2/Common/IAttrLang.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 11:05
+ * 
+ * License: GPL 2.1
+ */
+using System;
+using System.Globalization;
+
+namespace FB2.Common
+{
+	/// <summary>
+	/// Description of IAttrLang.
+	/// </summary>
+	public interface IAttrLang
+	{
+		/// <summary>
+        /// Language.
+        /// </summary>
+        CultureInfo AttrLang { set; get; }
+	}
+}
---
+++ Source/FB2/Common/IAttrValue.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Кузнецов Вадим (DikBSD)
+ * Date: 28.04.2009
+ * Time: 11:09
+ * 
+ * License: GPL 2.1
+ */
+using System;
+
+namespace FB2.Common
+{
+	/// <summary>
+	/// Description of IAttrValue.
+	/// </summary>
+	public interface IAttrValue
+	{
+		/// <summary>
+        /// Value of element.
+        /// </summary>
+        string AttrValue { get; set; }
+	}
+}
